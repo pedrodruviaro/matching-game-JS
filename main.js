6a76aea4ba81335f70c219030ed4e79df24b2b6e
Pedro Dalmolim Ruviaro <pedrodruviaro@gmail.com>
2021-07-24 10:30:01 -0300

    Componente CardGameFrontBack

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
-import BoardGame from './src/objects/BoardGame'
 import ScoreBoard from './src/objects/ScoreBoard'
+import BoardGame from './src/objects/BoardGame'
 
 
 
@@ -13,8 +13,6 @@ $root.insertAdjacentHTML(
     "beforeend",
     `   
         ${ScoreBoard()}
-        ${BoardGame(6)}
+        ${BoardGame(2)}
     `
-
-
 );
