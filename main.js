1b8e326554e85168341bd5ca2ca135a52069b29c
Pedro Dalmolim Ruviaro <pedrodruviaro@gmail.com>
2021-07-24 09:25:17 -0300

    Novo componente

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
+import BoardGame from './src/objects/BoardGame'
 
-import CardGame from "./src/components/CardGame"
+
 
 // $ => guarda elemento da tela
 const $root = document.querySelector("#app")
-const $htmlCardGame = CardGame()
+const $htmlBoardGame = BoardGame(6)
 
-$root.insertAdjacentHTML("beforeend" ,$htmlCardGame)
+$root.insertAdjacentHTML("beforeend" ,$htmlBoardGame)
